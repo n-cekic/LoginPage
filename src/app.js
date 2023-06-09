b99b13127e4cb6a8373953f8e9b23fac297d86a3
n-cekic <ncekic13@gmail.com>
2023-06-09 12:41:53 +0200

    handle sliding
    Keep "confirm password" visible if it has input, otherwise hide it.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
-let clickedOnSignIn = false;
-
-function submit() {
+async function submit() {
   console.log("Submitted!");
-  const username = document.getElementById("username").value;
+  const username = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   // Perform your login logic here
@@ -25,16 +23,23 @@ function submit() {
   console.log(resp);
 }
 
+let confirmPasswordVisible = false;
+
 function revealConfirmPassword() {
   console.log("sign in clicked.");
+
   const confPswd = document.getElementById("confirm-password");
   const buttons = document.getElementById("button-container");
-  buttons.style.transform = clickedOnSignIn
-    ? "translateY(0px)"
-    : "translateY(55px)";
-  clickedOnSignIn = !clickedOnSignIn;
-  //confPswd.style.opacity = confPswd.style.opacity === "1" ? "0" : "1";
 
-  //confPswd.classList.toggle("open");
+  if (!confirmPasswordVisible) {
+    buttons.style.transform = "translateY(55px)"
+    confirmPasswordVisible = true;
+  } else {
+    if (!confPswd.value){
+      buttons.style.transform = "translateY(0px)";
+      confirmPasswordVisible = false;
+    }
+  }
+
   console.log("confirm password revealed.");
 }
