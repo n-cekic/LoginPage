f15627d237dc17c2d9fec0a9ecc3edeff9d61e45
n-cekic <ncekic13@gmail.com>
2023-06-14 15:01:51 +0200

    tab size

--- src/app.js
+++ src/app.js
@@ -1,52 +1,52 @@
 async function login() {
-  console.log("Submitted!");
-  const username = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    console.log("Submitted!");
+    const username = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  // Perform your login logic here
-  // Check if input is correct
-  const data = {
-    username: username,
-    password: password,
-  };
+    // Perform your login logic here
+    // Check if input is correct
+    const data = {
+        username: username,
+        password: password,
+    };
 
-  const resp = await fetch("http://localhost:8080/login", {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+    const resp = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
 
-  console.log(resp);
+    console.log(resp);
 }
 
 let confirmPasswordVisible = false;
 
 function signin() {
-  console.log("sign in clicked.");
+    console.log("sign in clicked.");
 
-  const confPswd = document.getElementById("confirm-password");
-  const buttons = document.getElementById("button-container");
+    const confPswd = document.getElementById("confirm-password");
+    const buttons = document.getElementById("button-container");
 
-  if (!confirmPasswordVisible) {
-    revalConfirmPassword(buttons);
-  } else {
-    if (!confPswd.value) {
-      hideConfirmPassword(buttons);
+    if (!confirmPasswordVisible) {
+        revalConfirmPassword(buttons);
+    } else {
+        if (!confPswd.value) {
+            hideConfirmPassword(buttons);
+        }
     }
-  }
 }
 
 function revalConfirmPassword(buttons) {
-  buttons.style.transform = "translateY(55px)";
-  confirmPasswordVisible = true;
-  console.log("confirm password revealed.");
+    buttons.style.transform = "translateY(55px)";
+    confirmPasswordVisible = true;
+    console.log("confirm password revealed.");
 }
 
 function hideConfirmPassword(buttons) {
-  buttons.style.transform = "translateY(0px)";
-  confirmPasswordVisible = false;
-  console.log("confirm password hidden.");
+    buttons.style.transform = "translateY(0px)";
+    confirmPasswordVisible = false;
+    console.log("confirm password hidden.");
 }
