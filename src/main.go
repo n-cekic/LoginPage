79a08df4a969c824d7e3e10b33e5fc3f9e5be7a2
n-cekic <ncekic13@gmail.com>
2023-06-22 12:58:47 +0200

    write to database
    Process login data and write to database

--- src/main.go
+++ src/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
 
 // Struct of data to be recieved
 type LoginData struct {
@@ -36,7 +42,8 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Process loginData
-	fmt.Printf("Received login data: %+v\n", loginData)
+	fmt.Printf("Adding %+v to DB\n", loginData.Username)
+	addUser(loginData.Username, "", loginData.Password)
 
 	// print the request
 	res, err := httputil.DumpRequest(r, true)
@@ -52,6 +59,16 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func addUser(username string, email string, password string) {
+	if email == "" {
+		email = "TEMP_ADDRESS"
+	}
+	_, err := db.Exec("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", username, email, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	fmt.Println("Listening at port :8080")
 
@@ -60,5 +77,45 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./ui"))
 	mux.Handle("/", fs)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+
+	ctx, _ := context.WithCancel(context.Background())
+
+	go func() {
+		err := http.ListenAndServe(":8080", mux)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	cfg := mysql.Config{
+		User:                 "root",
+		Passwd:               "password",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "users",
+		AllowNativePasswords: true,
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	row := db.QueryRow("SELECT * FROM users WHERE UserID = 1;")
+
+	var id int
+	var uname string
+	var email string
+	var pswd string
+	row.Scan(&id, &uname, &email, &pswd)
+	fmt.Println(id, uname, email, pswd)
+
+	fmt.Println("Connected to the DB!")
+	<-ctx.Done()
 }
