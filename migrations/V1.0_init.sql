e3c42b25093b8ac6d228f9a7dc26442ad1716dbc
Nikola Cekić <nceki@Belca.local>
2023-12-03 15:53:29 +0100

    add migrations

---
+++ migrations/V1.0_init.sql
@@ -0,0 +1,12 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS `login`;
+
+-- Switch to the newly created database
+USE `login`;
+
+-- Create the 'user' table
+CREATE TABLE `user` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
