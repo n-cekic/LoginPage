1fa58aa44c6624839e175da4bb9fc58c8eb455f3
Nikola Cekic <ncekic13@gmail.com>
2023-12-03 20:36:14 +0100

    add salt to the db

--- migrations/V1.0_init.sql
+++ migrations/V1.0_init.sql
@@ -8,5 +8,6 @@ USE `login`;
 CREATE TABLE `user` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    salt BLOB NOT NULL
 );
